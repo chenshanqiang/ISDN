beb261fb775c0666a8bcb11160e1f8e0029a0342
chenshanqiang <chenshanqi.ang@163.com>
2018-07-25 15:08:50 +0800

    aa

--- js/security.js
+++ js/security.js
@@ -1,56 +1,57 @@
 // JavaScript Document
-var user = getCookiename( "username" ),
-pwd = getCookiepwd( "pass" ),
-usernameone = getCookienameone( "usernameone" ),
-pwdone = getCookiepwdone( "passone" );
-if ( user == "user" && pwd == "pwd" ) {} else if( usernameone=="dsppa" && pwdone=="dsppa" ){
-}else {
+var user = getCookiename("username"),
+	pwd = getCookiepwd("pass"),
+	usernameone = getCookienameone("usernameone"),
+	pwdone = getCookiepwdone("passone");
+if(user == "user" && pwd == "pwd") {} else if(usernameone == "dsppa" && pwdone == "dsppa") {} else {
 	parent.jumpmain();
 }
-function freThisPaper(){
-	 location.reload();
+
+function freThisPaper() {
+	location.reload();
 }
-function getCookiename( cname ) {
+
+function getCookiename(cname) {
 	var name = cname + "=";
-	var ca = document.cookie.split( ';' );
-	for ( var i = 0; i < ca.length; i++ ) {
-		var c = ca[ i ].trim();
-		if ( c.indexOf( name ) == 0 ) return c.substring( name.length, c.length );
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) {
+		var c = ca[i].trim();
+		if(c.indexOf(name) == 0) return c.substring(name.length, c.length);
 	}
 	return "";
 }
 
-function getCookiepwd( cpass ) {
+function getCookiepwd(cpass) {
 	var pass = cpass + "=";
-	var ca = document.cookie.split( ';' );
-	for ( var i = 0; i < ca.length; i++ ) {
-		var c = ca[ i ].trim();
-		if ( c.indexOf( pass ) == 0 ) return c.substring( pass.length, c.length );
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) {
+		var c = ca[i].trim();
+		if(c.indexOf(pass) == 0) return c.substring(pass.length, c.length);
 	}
 	return "";
 }
 
-function getCookienameone( cnameone ) {
+function getCookienameone(cnameone) {
 	var nameone = cnameone + "=";
-	var ca = document.cookie.split( ';' );
-	for ( var i = 0; i < ca.length; i++ ) {
-		var c = ca[ i ].trim();
-		if ( c.indexOf( nameone ) == 0 ) return c.substring( nameone.length, c.length );
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) {
+		var c = ca[i].trim();
+		if(c.indexOf(nameone) == 0) return c.substring(nameone.length, c.length);
 	}
 	return "";
 }
 
-function getCookiepwdone( cpassone ) {
+function getCookiepwdone(cpassone) {
 	var passone = cpassone + "=";
-	var ca = document.cookie.split( ';' );
-	for ( var i = 0; i < ca.length; i++ ) {
-		var c = ca[ i ].trim();
-		if ( c.indexOf( passone ) == 0 ) return c.substring( passone.length, c.length );
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) {
+		var c = ca[i].trim();
+		if(c.indexOf(passone) == 0) return c.substring(passone.length, c.length);
 	}
 	return "";
 }
 
-function setCookie( cname, cvaluename, cpass, cvaluepass,cnameone, cvaluenameone, cpassone, cvaluepassone ) {
+function setCookie(cname, cvaluename, cpass, cvaluepass, cnameone, cvaluenameone, cpassone, cvaluepassone) {
 	document.cookie = cname + "=" + cvaluename;
 	document.cookie = cpass + "=" + cvaluepass;
 	document.cookie = cnameone + "=" + cvaluenameone;
